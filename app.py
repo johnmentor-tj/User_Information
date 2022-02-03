3841012fefc98f2beb9452179fb92162018c8070
johnmentor-tj <81553651+johnmentor-tj@users.noreply.github.com>
2022-02-03 14:54:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('register.html')
+
+@app.route('/confirmation',methods = ['GET','POST'])
+def confirmation():
+
+    if request.method == 'POST':
+        n = request.form.get('name')
+        a = request.form.get('age')
+        s = request.form.get('salary')
+
+    return render_template('confirmation.html',name=n, age=a, salary=s)
+
+#Step - 1 Main Program
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
