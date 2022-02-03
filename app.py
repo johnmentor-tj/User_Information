a6018336ba01a0537bed42b3651956c473fadfc7
johnmentor-tj <81553651+johnmentor-tj@users.noreply.github.com>
2022-02-03 15:01:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('register.html')
 
-@app.route('/confirmation',methods = ['GET','POST'])
+@app.route('/confirm',methods = ['GET','POST'])
 def confirmation():
 
     if request.method == 'POST':
@@ -15,7 +15,7 @@ def confirmation():
         a = request.form.get('age')
         s = request.form.get('salary')
 
-    return render_template('confirmation.html',name=n, age=a, salary=s)
+    return render_template('confirm.html',name=n, age=a, salary=s)
 
 #Step - 1 Main Program
 if __name__ == '__main__':
